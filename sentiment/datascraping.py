e983cf4975c3b60d2249dde09eea19a86bedeff5
Keaton Xu <keatx@ku.edu>
2024-03-24 13:43:30 -0500

    Update datascraping.py
    Prints most common terms

--- sentiment/datascraping.py
+++ sentiment/datascraping.py
@@ -1,7 +1,7 @@
 import praw
 import string
-from preprocess import Preprocess
 from collections import Counter
+from preprocess import Preprocess
 
 KEY = "m3cPpLyaVlE0JxcZBkIFaA"
 SECRET = "Fvk4PgPavpfb8TR4wnKMMz5nF5a8HA"
@@ -15,17 +15,17 @@ reddit = praw.Reddit(client_id=KEY,
 stock = 'AAPL'
 
 terms = []
-for post in reddit.subreddit('wallstreetbets').search(stock,sort="relevance",limit=2):
-    terms.append(word for word in process.preprocess(post.title))
-    terms.append(word for word in process.preprocess(post.selftext))
+for post in reddit.subreddit('wallstreetbets').search(stock,sort="relevance",limit=5):
+    terms.extend(word for word in process.preprocess(post.title))
+    terms.extend(word for word in process.preprocess(post.selftext))
     for i in range(1,5):
-        terms.append(word for word in process.preprocess(post.comments[i].body))
+        terms.extend(word for word in process.preprocess(post.comments[i].body))
 
-stopword = list(string.punctuation) + ['AAPL']
-with open("english.txt", "r") as my_file:
+stopword = list(string.punctuation) + [stock.lower(),'“','”']
+with open("sentiment/english.txt", "r") as my_file:
     for line in my_file:
-        stopword.append(line)
+        stopword.append(line.strip())
 
-terms = [term for term in terms if term not in stopword]
+terms = [term for term in terms if term.lower() not in stopword]
 counter.update(terms)
-print(counter.most_common(5))
+print(counter.most_common(50))
