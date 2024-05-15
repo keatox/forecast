291de0737a28be70e36de9ceb07b64f92184d9f4
Keaton Xu <keatx@ku.edu>
2024-05-15 13:22:19 -0500

    Update datascraping.py
    moved remove stopwrod functionality away

--- src/sentiment/datascraping.py
+++ src/sentiment/datascraping.py
@@ -1,8 +1,6 @@
 import praw
-import string
 import os         
 from dotenv import load_dotenv, find_dotenv
-from collections import Counter
 from preprocess import Preprocess
 
 # you will need to create your own .env file and Reddit API app
@@ -18,20 +16,13 @@ reddit = praw.Reddit(client_id=KEY,
 def scrape(stock):
     terms = []
     for post in reddit.subreddit('wallstreetbets').search(stock,sort="relevance",limit=5):
-        terms.extend(word for word in process.preprocess(post.title))
-        terms.extend(word for word in process.preprocess(post.selftext))
+        terms.append(process.preprocess(post.title))
+        terms.append(process.preprocess(post.selftext))
         for i in range(1,5):
-            terms.extend(word for word in process.preprocess(post.comments[i].body))
+            terms.append(process.preprocess(post.comments[i].body))
 
-    stopword = list(string.punctuation) + [stock.lower(),'“','”']
-    with open("res/english.txt", "r") as my_file:
-        for line in my_file:
-            stopword.append(line.strip())
-
-    counter = Counter()
-    terms = [term for term in terms if term.lower() not in stopword]
-    counter.update(terms)
-    return(counter.most_common(50))
+    terms = process.remove_stopwords(terms,stock)
+    print(terms)
 
 stock = 'AAPL'
-print(scrape(stock))
+scrape(stock)
