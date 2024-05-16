18dbc7b4094b49b420e9557d4e6de9dc8430d167
Keaton Xu <keatx@ku.edu>
2024-05-16 14:41:04 -0500

    Update sentimodel.py
    Currently using LinearSVC, ~80% accuracy!

--- src/sentiment/sentimodel.py
+++ src/sentiment/sentimodel.py
@@ -2,7 +2,9 @@ from preprocess import Preprocess
 import numpy as np
 import pandas as pd
 import matplotlib as plt
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 
 class Sentimodel:
     def __init__(self):
@@ -11,18 +13,31 @@ class Sentimodel:
         self._X = self._data["review"].values.tolist()
         self._Y = self._data["sentiment"].values
 
-
+    def override(self,text):
+        return text
+    
     def process(self):
         for i in range(len(self._X)):
             self._X[i] = self._process.preprocess(self._X[i])
-        self._X = self._process.remove_stopwords(self._X)
+        self._X = self._process.remove_stopwords(self._X) 
+        self._X = self.vectorize(self._X)
+    
+    def vectorize(self,data):
+        vectorizer = CountVectorizer(max_features = 300,
+                                     preprocessor=self.override,
+                                     token_pattern='[a-zA-Z0-9$&+,:;=?@#|<>.^*()%!-]+')
+        return vectorizer.fit_transform(data).toarray()    
 
     def tune_model(self):
-        pass
+        X, Xt, Y, Yt = train_test_split(self._X, self._Y, test_size = 0.20, random_state = 0)
+
+        model = LinearSVC(dual='auto').fit(X,Y)
+        print(model.score(X,Y))
+        print(model.score(Xt,Yt))
 
     def return_model(self):
         pass
 
 model = Sentimodel()
-
 model.process()
+model.tune_model()
