754c0c66685d676f6174133d3fa0cf8e3d0f76b5
Keaton Xu <keatx@ku.edu>
2024-05-16 14:40:37 -0500

    Update datascraping.py
    added vectorization of terms such that it can be used in prediction model

--- src/sentiment/datascraping.py
+++ src/sentiment/datascraping.py
@@ -2,12 +2,14 @@ import praw
 import os         
 from dotenv import load_dotenv, find_dotenv
 from preprocess import Preprocess
+from sentimodel import Sentimodel
 
 # you will need to create your own .env file and Reddit API app
 load_dotenv(find_dotenv())
 KEY = os.getenv("REDDIT_API_KEY")
 SECRET = os.getenv("REDDIT_API_SECRET")
 
+model = Sentimodel()
 process = Preprocess()
 reddit = praw.Reddit(client_id=KEY,
                      client_secret=SECRET,
@@ -22,7 +24,9 @@ def scrape(stock):
             terms.append(process.preprocess(post.comments[i].body))
 
     terms = process.remove_stopwords(terms,stock)
-    print(terms)
+
+    terms = model.vectorize(terms)
+    print(terms.shape)
 
 stock = 'AAPL'
 scrape(stock)
