f9c7e2517aa8b8413561bd63b468c4df3347819d
Keaton Xu <keatx@ku.edu>
2024-05-18 14:47:45 -0500

    Update preprocess.py
    double underscore for private variables

--- src/sentiment/preprocess.py
+++ src/sentiment/preprocess.py
@@ -3,12 +3,12 @@ import string
 
 class Preprocess:
     def __init__(self):
-        self._emoticons = r"""(?:
+        self.__emoticons = r"""(?:
                               [:=;]             # Eyes
                               [oO\-]?           # Nose 
                               [D\)\]\(\]/\\OpP] # Mouth
                               )"""
-        self._regex = [self._emoticons,
+        self.__regex = [self._emoticons,
                        r'(?:@[\w_]+)',                   # @-mentions
                        r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
                        r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
@@ -20,11 +20,11 @@ class Preprocess:
     
     def tokenize(self,s):    
         s = re.sub(r'<[^>]+>',"",s)
-        tokens_re = re.compile(r'('+'|'.join(self._regex)+')', re.VERBOSE | re.IGNORECASE)
+        tokens_re = re.compile(r'('+'|'.join(self.__regex)+')', re.VERBOSE | re.IGNORECASE)
         return tokens_re.findall(s)
     
     def preprocess(self,s, lowercase=False):
-        emoticon_re = re.compile(r'^'+self._emoticons+'$', re.VERBOSE | re.IGNORECASE)
+        emoticon_re = re.compile(r'^'+self.__emoticons+'$', re.VERBOSE | re.IGNORECASE)
         tokens = self.tokenize(s)
         if lowercase:
             tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
