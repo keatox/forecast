90831dda65e83d5b59b906bbad25824671d4df23
Keaton Xu <keatx@ku.edu>
2024-05-20 18:18:19 -0500

    Update datascraping.py
    commenting

--- src/sentiment/datascraping.py
+++ src/sentiment/datascraping.py
@@ -16,21 +16,26 @@ reddit = praw.Reddit(client_id=KEY,
                      user_agent='Sentiment analysis')
 
 def scrape(stock):
-    terms = []
+    terms = []                                                                   # change limit to determine number of posts to scrape
     for post in reddit.subreddit('wallstreetbets').search(stock,sort="relevance",limit=100):
         terms.append([post.selftext])
     terms = [term for term in terms if term[0] != "" and len(term[0].split()) >= 15]  # removes empty terms to mitigate false negatives
 
+    # processing and prediction
     tokens = [process.preprocess(term)[0] for term in terms]
     counts = model.vectorize(tokens)
     predictions = model.prediction(counts)
 
+    # prints each term and respective prediction
     [print(terms[i][0],[predictions[i].upper()]) for i in range(len(predictions))]
+    
+    # prints sentiment score out of 10
     sum = 0
     for i in range(len(predictions)):
         if predictions[i] == 'positive':
             sum += 1
-    print(sum/len(predictions))
+    print("%.2f" % (10 * sum/len(predictions)))
 
+# hardcoded stock for now
 stock = 'DOGE'
 scrape(stock)
