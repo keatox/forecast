5ef34ef3c337277c41e175d5e9b9b0dca1ee1ae4
Keaton Xu <keatx@ku.edu>
2024-05-20 18:01:59 -0500

    Update preprocess.py
    commenting

--- src/sentiment/preprocess.py
+++ src/sentiment/preprocess.py
@@ -19,22 +19,26 @@ class Preprocess:
                        ]
         self.__stopwords = self.init_stopwords()
 
+    # returns list of processed terms
     def preprocess(self,s):
         tokens = [self.tokenize(phrase) for phrase in s]
         return self.remove_stopwords(tokens)
     
+    # creates a token of certain phrases e.g. @s, links, or URLs
     def tokenize(self,s):    
         s = re.sub(r'<[^>]+>',"",s)
         tokens_re = re.compile(r'('+'|'.join(self.__regex)+')', re.VERBOSE | re.IGNORECASE)
         return tokens_re.findall(s)
     
+    # loads stopwords from .txt file
     def init_stopwords(self):
         stopword = list(string.punctuation)
-        with open("res/english.txt", "r") as my_file:
+        with open("res/stopwords.txt", "r") as my_file:
             for line in my_file:
                 stopword.append(line.strip())
         return stopword
     
+    # iterates through each term in terms list and removes stopwords
     def remove_stopwords(self,terms):
         for i in range(len(terms)):
             terms[i] = " ".join([term for term in terms[i] if term.lower() not in self.__stopwords]).lower()
