2c7f148705ae7bdfdecd5004361842f00ebefe4e
Keaton Xu <keatx@ku.edu>
2024-05-28 21:12:26 -0500

    Update predict.py
    testing

--- src/backend/predict.py
+++ src/backend/predict.py
@@ -5,56 +5,63 @@ import pandas as pd
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import seaborn as sns
-from datascraping import Datascraping
 
-scraper = Datascraping()
+class Predict:
+    def __init__(self):
+        self.__prices = None
 
-#add error handling for invalid stock etc
-stock = 'AAPL'
+    def predict_prices(self,stock,days=85,iters=10000):
+        price_paths = self.compute_returns(stock,days,iters)
+        prediction = price_paths.mean(axis=1)[-1]
+        print(f'Og price: ${self.__prices.iloc[-days].values[0]}')
+        print(f"Predicted: ${prediction}")
+        print(f"Actual: ${self.__prices.iloc[-1].values[0]}")
+        print('Percent Change: %.2f' % float(100 * ((prediction - self.__prices.iloc[-days])/self.__prices.iloc[-days]).values[0])+'%')
 
-ticker = yf.Ticker(stock)
+        from sklearn.metrics import mean_absolute_error, mean_squared_error
+        mae = mean_absolute_error(self.__prices.iloc[-days-1:-1], price_paths.mean(axis=1))
+        mse = mean_squared_error(self.__prices.iloc[-days-1:-1], price_paths.mean(axis=1))
+        rmse = np.sqrt(mse)
+        print(f'Mean Absolute Error (MAE): {mae}')
+        print(f'Mean Squared Error (MSE): {mse}')
+        print(f'Root Mean Squared Error (RMSE): {rmse}')
 
-prices = pd.DataFrame()
-prices[stock] = ticker.history(period='1y')['Close']
+        plt.style.use("dark_background")
+        plt.figure(figsize=(15,5))
+        plt.plot(self.__prices)
+        new_dates = [self.__prices[stock].index[-days] + timedelta(days=i) for i in range(0,days)]
+        num_graphs = 0
+        for i in range(len(price_paths)):
+            if num_graphs < 5 and abs(price_paths[:,i][-1] - prediction) <= 0.1 * prediction:
+                plt.plot(new_dates,price_paths[:,i],alpha=0.35)
+                num_graphs += 1
+        plt.tight_layout()
+        plt.show()
 
-log_return = np.log(1 + prices.pct_change())
-u = log_return.mean()
-var = log_return.var()
-drift = u - (0.5*var)
+    def get_prices(self,stock):
+        ticker = yf.Ticker(stock)
+        prices = pd.DataFrame()
+        prices[stock] = ticker.history(period='1y')['Close']
+        self.__prices = prices
 
-stdev = log_return.std()
-days = 85
-iters = 10000
-Z = norm.ppf(np.random.rand(days, iters)) 
-daily_returns = np.exp(drift.values + stdev.values * Z)
+    def compute_returns(self,stock,days,iters):
+        self.get_prices(stock)
+        log_return = np.log(1 + self.__prices.pct_change())
+        u = log_return.mean()
+        var = log_return.var()
+        drift = u - (0.5*var)
+        stdev = log_return.std()
 
-price_paths = np.zeros_like(daily_returns)
-price_paths[0] = prices.iloc[-60]
-for i in range(1, days):
-    price_paths[i] = price_paths[i-1]*daily_returns[i]
+        rng = np.random.default_rng()
+        Z = norm.ppf(rng.uniform(size=(days, iters)))
+        daily_returns = np.exp(drift.values + stdev.values * Z)
 
-prediction = price_paths.mean(axis=1)[-1]
-print(f'Og price: ${prices.iloc[-60].values[0]}')
-print(f"Predicted: ${prediction}")
-print(f"Actual: ${prices.iloc[-1].values[0]}")
-print('Percent Change: %.2f' % float(100 * ((prediction - prices.iloc[-60])/prices.iloc[-60]).values[0])+'%')
+        price_paths = np.zeros_like(daily_returns)
+        price_paths[0] = self.__prices.iloc[-days]
+        for i in range(1, days):
+            price_paths[i] = price_paths[i-1]*daily_returns[i]
+        return price_paths
 
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-mae = mean_absolute_error(prices.iloc[-days-1:-1], price_paths.mean(axis=1))
-mse = mean_squared_error(prices.iloc[-days-1:-1], price_paths.mean(axis=1))
-rmse = np.sqrt(mse)
-
-print(f'Mean Absolute Error (MAE): {mae}')
-print(f'Mean Squared Error (MSE): {mse}')
-print(f'Root Mean Squared Error (RMSE): {rmse}')
-
-plt.figure(figsize=(15,6))
-plt.plot(prices)
-new_dates = [prices[stock].index[-60] + timedelta(days=i) for i in range(0,days)]
-num_graphs = 3
-for i in range(len(price_paths)):
-    if num_graphs <= 5 and abs(price_paths[:,i][-1] - prediction) <= prediction/(prediction // 10):
-        plt.plot(new_dates,price_paths[:,i],alpha=0.35)
-        num_graphs += 1
-plt.show()
+stock = 'NVDA'
+model = Predict()
+model.predict_prices(stock,90)
