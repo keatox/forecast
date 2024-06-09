7f70538ab7625a166b93f13c726b67e532a294ea
Keaton Xu <keatx@ku.edu>
2024-06-08 22:39:58 -0500

    Update scripts.js
    darkmode for line break

--- src/frontend/static/scripts.js
+++ src/frontend/static/scripts.js
@@ -1,19 +1,3 @@
-function toggleDarkmode(){
-    var body = document.body;
-    var blocks = document.querySelectorAll('#block');
-
-    body.classList.toggle("darkmodebody");
-    for (var i=0;i<blocks.length;i++){
-        blocks[i].classList.toggle("darkmodeblock");
-    }
-
-    if (body.classList.contains("darkmodebody")) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-}
-
 // function handle(input){
 //     if (input.keyCode === 13){
 //         input.preventDefault();
@@ -32,10 +16,36 @@ function toggleDarkmode(){
 //     }
 // }
 
+// closes dropdown everytime a new page is loaded
+// so that if one backtracks pages it is closed
 function closeMenu(){
     document.getElementById('menu').checked = false;
 }
 
+// toggles all necessary items 
+function toggleDarkmode(){
+    var body = document.body;
+    var blocks = document.querySelectorAll('#block');
+    var border = document.getElementById('linebreak');
+
+    try{
+        border.classList.toggle("darklinebreak");
+    } catch(error){
+    }
+    body.classList.toggle("darkmodebody");
+    for (var i=0;i<blocks.length;i++){
+        blocks[i].classList.toggle("darkmodeblock");
+    }
+
+    // saves toggle state
+    if (body.classList.contains("darkmodebody")) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+// checks toggle state upon loading new page to keep light/dark mode
 document.addEventListener('DOMContentLoaded', (event) => {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
