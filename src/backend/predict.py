361fd7ce1d2b7d8cd1f1ffae0b92ac2a1057e751
Keaton Xu <keatx@ku.edu>
2024-06-10 00:33:19 -0500

    Update predict.py
    subbed matplot/sns for plotly

--- src/backend/predict.py
+++ src/backend/predict.py
@@ -3,12 +3,14 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from jinja2 import Template
 
 class Predict:
     def __init__(self):
         self.__prices = None
+        self.__tickers = np.loadtxt('res/nasdaq.csv',skiprows=1,usecols=(0,1,5,6,8,9),dtype=str,delimiter=',')
 
     def predict_prices(self,stock,days=85,iters=10000):
         price_paths = self.compute_returns(stock,days,iters)
@@ -26,17 +28,29 @@ class Predict:
         print(f'Mean Squared Error (MSE): {mse}')
         print(f'Root Mean Squared Error (RMSE): {rmse}')
 
-        plt.style.use("dark_background")
-        plt.figure(figsize=(15,5))
-        plt.plot(self.__prices)
+        plt = px.line(self.__prices)
+
         new_dates = [self.__prices[stock].index[-days] + timedelta(days=i) for i in range(0,days)]
         num_graphs = 0
         for i in range(len(price_paths)):
-            if num_graphs < 5 and abs(price_paths[:,i][-1] - prediction) <= 0.1 * prediction:
-                plt.plot(new_dates,price_paths[:,i],alpha=0.35)
+            if num_graphs < 3 and abs(price_paths[:,i][-1] - prediction) <= 0.1 * prediction:
+                plt.add_trace(go.Scatter(x=new_dates, y=price_paths[:,i],mode='lines',opacity=0.35))
                 num_graphs += 1
-        plt.tight_layout()
+        plt.add_vline(x=self.__prices[stock].index[-days],line_color='gray',line_dash='dash')
+        plt.update_layout(showlegend=False,
+                          yaxis_title=None,
+                          xaxis_title=None,
+                          paper_bgcolor='rgba(0,0,0,0)',
+                          plot_bgcolor='rgba(0,0,0,0)',
+                          margin=dict(l=20, r=20, t=20, b=20),
+                          autosize=True)
+
         plt.show()
+        
+        return {'chart': plt.to_html(full_html=False,config={'displayModeBar': False}),
+                'initial': 5,
+                'predicted': 7,
+               }
 
     def get_prices(self,stock):
         ticker = yf.Ticker(stock)
