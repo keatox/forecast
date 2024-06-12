329b01cbcd5395cc896a1d2f48bd13648817ab14
Keaton Xu <keatx@ku.edu>
2024-06-11 22:57:05 -0500

    Update app.py
    remove globals, condense code

--- src/backend/app.py
+++ src/backend/app.py
@@ -6,68 +6,45 @@ scraper = Datascraping()
 model = Predict()
 app = Flask(__name__,template_folder='../frontend/templates',static_folder='../frontend/static')
 
-stock=''
-data = {}
-pred = {}
-new_data = False
-
 @app.route('/',methods=['GET','POST'])
 def main():
-    global stock
-    global new_data
     if request.method == 'POST':
         query = request.form
         # checks which search bar is being used
         if query.get('landingsearch'):
             stock = query.get('landingsearch').upper()
             if scraper.is_valid_stock(stock):
-                new_data = True
-                return dashboard()
+                return dashboard(True)
             else:
-                return render_template('landing.html',error="unknown stock ticker")
-        elif query.get('dashsearch'):
-            tempstock = query.get('dashsearch').upper()
-            if scraper.is_valid_stock(tempstock):
-                stock = tempstock
-                new_data = True
-            return dashboard(not new_data)
+                return landing(True)
+        else:
+            stock = query.get('dashsearch').upper()
+            boolean = scraper.is_valid_stock(stock)
+            return dashboard(boolean, not boolean)
     # default landing
     else:
-        return render_template('landing.html')
+        return landing()
+    
+@app.route('/landing')
+def landing(error=False):
+    return render_template('landing.html',error=error)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/dashboard',methods=['GET','POST'])
-def dashboard(error=False):
-    global data
-    global pred
-    global new_data
+@app.route('/dashboard')
+def dashboard(new_data=False,error=False):
     # generates new dash if new stock is inputted
     if new_data:
-        data = scraper.scrape(stock)
-        pred = model.predict_prices(stock)
-        new_data = False
+        scraper.scrape(scraper.stock)
+        model.predict_prices(scraper.stock)
     return render_template('dashboard.html',
                            error=error,
-                           ticker=stock,
-                           score=data['score'],
-                           positive=data['positive'],
-                           negative=data['negative'],
-                           chart=pred['chart'],
-                           fullname=pred['fullname'],
-                           pprice=pred['predicted'],
-                           oprice=pred['initial'],
-                           change=pred['change'],
-                           high=pred['fiftyTwoWeekHigh'],
-                           low=pred['fiftyTwoWeekLow'],
-                           country=pred['country'],
-                           sector=pred['sector'],
-                           industry=pred['industry'],
-                           volume=pred['volume'],
-                           markcap=pred['markcap'])
-
+                           ticker=scraper.stock,
+                           data=scraper.data,
+                           pred=model.info)
+    
 @app.route('/help')
 def help():
     return render_template('help.html')
