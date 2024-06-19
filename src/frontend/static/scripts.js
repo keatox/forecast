daa81caa5a8f6c90222b4e0492bc7c0bbe5a574f
Keaton Xu <keatx@ku.edu>
2024-06-19 14:57:11 -0500

    Updates scripts.js

--- src/frontend/static/scripts.js
+++ src/frontend/static/scripts.js
@@ -1,5 +1,4 @@
-// closes dropdown everytime a new page is loaded
-// so that if one backtracks pages it is closed
+// closes dropdown everytime a new page is loaded so if user backtracks pages it is closed
 function closeMenu(){
     document.getElementById('menu').checked = false;
 }
@@ -38,3 +37,64 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }  
 }); 
+
+// queries db and lists top suggestions
+document.addEventListener('DOMContentLoaded', () => {
+    const landing = document.getElementById('landingsearch');
+    const dash = document.getElementById('dashsearch');
+    const resultsContainer = document.getElementById('results');
+    const searchForm = document.getElementById('searchForm');
+
+    if (landing){
+        var searchInput = landing;
+    } else{
+        var searchInput = dash;
+    }
+    
+    searchInput.addEventListener('input', function() {
+        const query = this.value;
+        if (query.length > 0) {
+            fetchSuggestions(query);
+        } else {
+            clearSuggestions();
+        }
+    });
+
+    // fetches top suggestions from autocomplete
+    function fetchSuggestions(query) {
+        fetch(`/autocomplete?query=${encodeURIComponent(query)}`)
+            .then(response => response.json())
+            .then(data => {
+                displaySuggestions(data);
+            })
+            .catch(error => {
+                console.error('Error fetching autocomplete suggestions:', error);
+            });
+    }
+
+    // creates new li tags for each suggestion
+    function displaySuggestions(suggestions) {
+        clearSuggestions();
+        if (suggestions.length > 0) {
+            resultsContainer.classList.remove('hidden');
+            suggestions.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = item;
+                li.addEventListener('click', () => {
+                    searchInput.value = item;
+                    clearSuggestions();
+                    searchForm.submit();
+                });
+                resultsContainer.appendChild(li);
+            });
+        } else {
+            resultsContainer.classList.add('hidden');
+        }
+    }
+
+    // clears and hides dropdown as needed 
+    function clearSuggestions() {
+        resultsContainer.innerHTML = '';
+        resultsContainer.classList.add('hidden');
+    }
+});
