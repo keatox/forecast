9b7501d2d472a4695b30dafd56cc9344f4929d73
Keaton Xu <keatx@ku.edu>
2024-06-24 13:19:49 -0500

    Update app.py
    autocomplete routing

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from datascraping import Datascraping
+from database import Database
 from predict import Predict
 
 scraper = Datascraping()
 model = Predict()
+db = Database()
 app = Flask(__name__,template_folder='../frontend/templates',static_folder='../frontend/static')
 
 @app.route('/',methods=['GET','POST'])
@@ -12,14 +14,14 @@ def main():
         query = request.form
         # checks which search bar is being used
         if query.get('landingsearch'):
-            stock = query.get('landingsearch').upper()
-            if scraper.is_valid_stock(stock):
+            stock = query.get('landingsearch').upper().split()[-1].replace('(','').replace(')','')
+            if db.is_valid_stock(stock):
                 return dashboard(True)
             else:
                 return landing(True)
         else:
-            stock = query.get('dashsearch').upper()
-            boolean = scraper.is_valid_stock(stock)
+            stock = query.get('dashsearch').upper().split()[-1].replace('(','').replace(')','')
+            boolean = db.is_valid_stock(stock)
             return dashboard(boolean, not boolean)
     # default landing
     else:
@@ -37,11 +39,11 @@ def about():
 def dashboard(new_data=False,error=False):
     # generates new dash if new stock is inputted
     if new_data:
-        scraper.scrape(scraper.stock)
-        model.predict_prices(scraper.stock)
+        scraper.scrape(db.stock)
+        model.predict_prices(db.stock)
     return render_template('dashboard.html',
                            error=error,
-                           ticker=scraper.stock,
+                           ticker=db.stock,
                            data=scraper.data,
                            pred=model.info)
     
@@ -49,6 +51,26 @@ def dashboard(new_data=False,error=False):
 def help():
     return render_template('help.html')
 
+# gets query from frontend and returns top results
+@app.route('/autocomplete', methods=['GET'])
+def autocomplete():
+    query = request.args.get('query', '').strip()
+    if query:
+        try:
+            conn = db.connect_db()
+            cursor = conn.cursor()
+            cursor.execute("""SELECT * FROM Stocks 
+                            WHERE UPPER(ticker) LIKE UPPER(%s)
+                            OR UPPER(name) LIKE UPPER(%s)
+                            LIMIT 5;""",(query + '%',query + '%'))
+            results = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return jsonify([result[1] + " ("+ result[0] +")" for result in results])
+        except:
+            print("Can't connect to server.")
+    return jsonify([])
+
 if __name__ == '__main__':
-    # app.run(debug=True,host='0.0.0.0')
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
+    # app.run(debug=True)
